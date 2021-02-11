6831416a0307470b5590d6f15e358b295b7832be
entchev <micishi@gmail.com>
2021-02-11 19:22:45 +0000

    update

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,25 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import useResults from '../hooks/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [searchApi, results, errorMsg] = useResults()
 
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price
+    })
+  }
+
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       ></SearchBar>
       {errorMsg ? <Text>{errorMsg}</Text> : null}
-      <Text>We have found {results.length} results</Text>
+      <Text style={styles.text}>We have found {results.length} results</Text>
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice('£')}
+          title='Cost Effective'
+        />
+        <ResultsList results={filterResultsByPrice('££')} title='Bit Pricier' />
+        <ResultsList
+          results={filterResultsByPrice('£££')}
+          title='Big Spender'
+        />
+        <ResultsList results={filterResultsByPrice('££££')} title='Whale' />
+      </ScrollView>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text: {
+    marginLeft: 15,
+  },
+})
 
 export default SearchScreen
