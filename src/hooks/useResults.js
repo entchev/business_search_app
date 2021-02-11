4e6e4c0d52a98c4cf3010a043cf3362bee4776fe
entchev <micishi@gmail.com>
2021-02-11 17:36:37 +0000

    first commit

---
+++ src/hooks/useResults.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import yelp from '../api/yelp'
+
+export default () => {
+  const [results, setResults] = useState([])
+  const [errorMsg, setErrorMsg] = useState('')
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'london',
+        },
+      })
+      setResults(response.data.businesses)
+    } catch (e) {
+      setErrorMsg('Something went wrong..')
+    }
+  }
+
+  useEffect(() => {
+    searchApi('Denis')
+  }, [])
+
+  return [searchApi, results, errorMsg]
+}
